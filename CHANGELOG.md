47b1b4f3c515f7c1ab9fc74a2ee5f4084c83aba6
Lance Albertson <lance@osuosl.org>
2014-08-18 17:20:42 -0700

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of ganeti-instance-image
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ganeti-instance-image'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures ganeti-instance-image'
+long_description 'Installs/Configures ganeti-instance-image'
+version          '0.1.0'
+
