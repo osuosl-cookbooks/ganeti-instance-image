a83d6b9f61059d9642831e11f1a41cc60efa845f
Lance Albertson <lance@osuosl.org>
2015-06-05 15:09:35 -0700

    Import cluster setup into local test cookbook and call it directly in TK

--- test/cookbooks/test-instance-image/recipes/default.rb
+++ test/cookbooks/test-instance-image/recipes/default.rb
@@ -1,3 +1,18 @@
+# XXX: This recipe is only used for testing and should not be used otherwise.
+# Setup a fake ganeti cluster ip that's resolvable
+hostsfile_entry '192.168.125.10' do
+  hostname 'ganeti.local'
+  action :create
+end
+# Make sure the hostname isn't set to localhost
+# NOTE: This usually happens on vagrant systems
+hostsfile_entry node['network']['interfaces']['eth0']['addresses'].keys[1] do
+  hostname node['fqdn']
+  aliases [node['hostname']]
+  unique true
+  action :create
+end
+
 include_recipe 'ganeti-instance-image'
 
 ganeti_instance_image_subnet 'vlan100' do
